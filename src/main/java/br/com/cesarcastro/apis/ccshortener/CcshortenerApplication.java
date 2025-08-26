0bc04a10504fc700eb3e499d6e2e0eb6567ae463
Cesar Castro <ccastro@cesarcastro.com.br>
2025-08-26 10:22:10 -0300

    refactor(banner): Adicionado banner e melhorado informações sobre a api

--- src/main/java/br/com/cesarcastro/apis/ccshortener/CcshortenerApplication.java
+++ src/main/java/br/com/cesarcastro/apis/ccshortener/CcshortenerApplication.java
@@ -3,12 +3,21 @@ package br.com.cesarcastro.apis.ccshortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneId;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
 
 @SpringBootApplication
 public class CcshortenerApplication {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
-        SpringApplication.run(CcshortenerApplication.class, args);
+
+        Map<String, Object> props = new HashMap<>();
+        props.put("system.timezone", ZoneId.systemDefault().toString());
+
+        SpringApplication app = new SpringApplication(CcshortenerApplication.class);
+        app.setDefaultProperties(props);
+        app.run(args);
     }
 }
