e1facfaa298de14c58442fe7f31dcfb18a1c3d24
Cesar Castro <ccastro@cesarcastro.com.br>
2025-09-02 17:16:33 -0300

    test(E2E): Adicionado teste e2e

--- src/e2e/java/br/com/cesarcastro/apis/ccshortener/ShortenerE2ETest.java
+++ src/e2e/java/br/com/cesarcastro/apis/ccshortener/ShortenerE2ETest.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.net.URI;
 
